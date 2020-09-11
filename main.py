a60676f5e0267aabecd14c020642cd50d07688f3
Doug Fresh <dougiefresh1513@gmail.com>
2020-09-10 20:38:28 -0400

    Project Commit

--- main.py
+++ main.py
@@ -5,16 +5,20 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from bs4 import BeautifulSoup
-import selenium
+from selenium import webdriver
+from selenium.webdriver import Chrome, ChromeOptions
 import requests
+from prettyprinter import pprint, cpprint
+from getpass import getpass
+import time
+
 
 class SimpleAT:
 
-    def __init__(self, profile, log_url, open_tasks):
-        # includes, password, username, email, phone_number, card_info
-        self.profile = profile
+    def __init__(self, log_url, open_tasks={}):
         # should just be the initial url to get to for the website
         self.log_url = log_url
+
 
         self.open_tasks = open_tasks
 
@@ -24,36 +28,58 @@ class SimpleAT:
     # def startSession(self, cookieItem):
     #     self.s.cookies =
 
-    def setLogin(self):
+    def setLogin(self, user_info):
+        # USER_INFO includes, password, username, email, phone_number, card_info
+
         # storeLogins in a database or something similar
         # TODO: Always check for errors, delete current "session" data and form data
         with requests.Session() as sess:
-            log_page = sess.get(self.log_url)
-            log_form = BeautifulSoup(log_page.text, 'lxml')
-            field_uname = log_form.find('input', {'name': 'username'})
-            if field_uname is None:
-                field_uname = log_form.find('input', {'name': 'email'})
-
+            # log_page = sess.get(self.log_url)
+            log_into_page = sess.post(self.log_url, user_info)
+            pprint(log_into_page.text)
+            api_cookies = log_into_page.cookies
+            print(api_cookies)
             # if login and data.cookieSessions is determined
             #  try to start session with cookies
             # elseif login and data.form_data is determined try to login
             # else try to login through random attempts
-            field_pass = log_form.find('input', {'name': 'password'})
-            print(field_uname, field_pass, self.profile)
+            # if the login request fails try the next soup form html
+            # set proper cookies
+            # insert a genuine referer code
 
-#    def getFormData(self, postRequest):
+            # headers = {'Referer': 'https://'}
+            # use another url if available on form
+
+    def grabCookies(self, url):
+        with requests.Session() as sess:
+            cookiesPage = sess.get(url)
+
+    def getLoginPageForm(self, user_info):
+        d = webdriver.Chrome(executable_path='drivers/chromedriver', options={"enable_automation"})
+        d.get(self.log_url)
+        d.find_element_by_name('username').send_keys(user_info['username'])
+        d.find_element_by_name('password').send_keys(user_info['password'])
+
+        d.find_element_by_class_name('signIn-btn').click()
+        time.sleep(40)
+        d.quit()
+
+        # log_form = BeautifulSoup(log_page.text, 'lxml')
+        # field_uname = log_form.find('input', {'name': 'username'})
+        # if field_uname is None:
+        #     field_uname = log_form.find('input', {'name': 'email'})
+        # field_pass = log_form.find('input', {'name': 'password'})
 
 
-user_info = {'password': '6106371972', 'username': 'aj@moserhomes.com'}
+#headers={"Referere"}
+#get user info from a selector and data
+user_info = dict(password='6106371972', username= 'aj@moserhomes.com')
+#same for login url
 login_url = 'https://app.buildtopia.com/english_exec/login'
-obj = SimpleAT(user_info, login_url)
-obj.setLogin()
+obj = SimpleAT(login_url)
+obj.getLoginPageForm(user_info)
 
-    # login
-    #loginRequest = requests.post()
 
-    # if the login request fails try the next soup form html
-    # set proper cookies
 
 #def theSoup(text):
 #   html = BeautifulSoup(text, 'lxml')
